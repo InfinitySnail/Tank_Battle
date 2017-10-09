f8dd6a678a52d547c1c41322b747e829bf9f4c48
Peter Bauman <peter@gregbauman.net>
2017-10-09 14:13:26 -0500

    Disabled gravity on all parts except the main body of the tank.

--- TankBattle/Source/TankBattle/Public/TankBarrel.h
+++ TankBattle/Source/TankBattle/Public/TankBarrel.h
@@ -7,7 +7,7 @@
 #include "TankBarrel.generated.h"
 
 
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent))
 class TANKBATTLE_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
