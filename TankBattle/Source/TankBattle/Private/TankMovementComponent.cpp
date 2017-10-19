9c71ff518fe0680003cfb9d00abf998f22378f07
Peter Bauman <peter@gregbauman.net>
2017-10-19 13:08:00 -0500

    Cleaned up code - removed old, commented-out logs that were no longer needed and removed whitespace

--- TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
+++ TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
@@ -7,21 +7,15 @@
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-
-
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
-	RightTrack->SetThrottle(-Throw);
-	// TODO prevent double-speed due to dual control use
+	RightTrack->SetThrottle(-Throw);	
 }
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
@@ -36,16 +30,10 @@ void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity, boo
 	// No need to call Super since we're replacing the functionality
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
-	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveForward(ForwardThrow);
 
 	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
-
 	IntendTurnRight(RightThrow);
-
-
-	// auto TankName = GetOwner()->GetName();
-	// UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *AIForwardIntention);
-
 }
--- TankBattle/Source/TankBattle/Public/TankMovementComponent.h
+++ TankBattle/Source/TankBattle/Public/TankMovementComponent.h
@@ -12,20 +12,20 @@ class TANKBATTLE_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
-		UFUNCTION(BlueprintCallable, Category = Input)
+		UFUNCTION(BlueprintCallable, Category = "Input")
 		void IntendMoveForward(float ControlThrow); // ControlThrow is how far forward or back the thumbstick is moved
 
-		UFUNCTION(BlueprintCallable, Category = Input)
+		UFUNCTION(BlueprintCallable, Category = "Input")
 		void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 
-		UFUNCTION(BlueprintCallable, Category = Input)
+		UFUNCTION(BlueprintCallable, Category = "Input")
 		void IntendTurnRight(float Throw);
 
-		// TODO check best protection
-		virtual void RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
-	
+
+	// Called from the AI pathfinding logic by the AI controllers
+	virtual void RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed) override;
+
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 };
